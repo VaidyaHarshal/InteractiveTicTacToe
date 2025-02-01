6c7e412c556adc6a42194f7a5c61da4df6171768
Harshal Vaidya <harshal.vaidya300@gmail.com>
2025-02-01 12:23:49 -0500

    initializing repository

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Board from "./components/Board";
+import Game from "./components/Game";
+
+function App() {
+  return (
+    <>
+      <Board />
+      <Game />
+    </>
+  );
+}
+
+export default App;
