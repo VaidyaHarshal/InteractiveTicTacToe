91ec4de0a8925e3859fb72bad00b9fda91099567
Harshal Vaidya <harshal.vaidya300@gmail.com>
2025-02-01 18:08:18 -0500

    add logic for board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const Board = () => {
-  return <div>Board</div>;
+const Board = ({ squares, onClick }) => {
+  return (
+    <div className="board">
+      {squares.map((value, index) => (
+        <button
+          key={index}
+          className={`square ${value || ""} ${
+            value ? (value === "X" ? "X" : "O") : ""
+          }`}
+          onClick={() => onClick(index)}
+        >
+          {value}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default Board;
